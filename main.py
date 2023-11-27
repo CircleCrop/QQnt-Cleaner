36416c3ad74dcb9f2de9e78b57c18fff65a7a532
CircleCrop <circlecrop@outlook.com>
2023-11-27 13:42:17 +0800

    function

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import os
 import re
 import shutil
+import logging
 
-root_folder = r"C:\Users\CCrop\Documents\Tencent Files"  # eg: X:\xxx\xxx\Tencent Files
+root_folder = r"C:\Users\CCrop\Documents\Tencent Files test"  # eg: X:\xxx\xxx\Tencent Files
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def get_numeric_folders(root_path):
+def get_numeric_folders(root_path):  # 获取 QQ 号根路径
     numeric_folders = []
     for folder_name in os.listdir(root_path):
         if os.path.isdir(os.path.join(root_path, folder_name)):
             if re.fullmatch(r'\d+', folder_name):  # 检查文件夹名是否全为数字
                 numeric_folders.append(root_path + "\\" + folder_name)
+                logging.debug("Find QQ number: " + folder_name)
     return numeric_folders
 
 
-def delete_empty_subfolders(path):
+def delete_empty_subfolders(path):  # 遍历删除空的子文件夹
     if not os.path.isdir(path):
         return
     for root, dirs, files in os.walk(path, topdown=False):
@@ -22,10 +25,11 @@ def delete_empty_subfolders(path):
             dir_path = os.path.join(root, dir)
             if not os.listdir(dir_path):  # 检查文件夹是否为空
                 os.rmdir(dir_path)  # 删除空文件夹
-    print(f"Del empty folder: {path}\\*")
+                logging.debug("Del: " + dir_path)
+    logging.info(f"Del empty folder: {path}\\*")
 
 
-def del_path_all(path):
+def del_path_all(path):  # 删除文件夹所有内容
     if os.path.exists(path):
         # 遍历目录中的所有内容
         for item in os.listdir(path):
@@ -34,10 +38,11 @@ def del_path_all(path):
                 os.remove(item_path)  # 删除文件
             elif os.path.isdir(item_path):
                 shutil.rmtree(item_path)  # 删除文件夹及其所有内容
-    print(f"Del all: {path}\\*")
+                logging.debug("Del: " + item_path)
+    logging.info(f"Del all: {path}\\*")
 
 
-def delete_dot_prefixed_folders(path):
+def delete_dot_prefixed_folders(path):  # 删除 . 开头子文件夹
     if not os.path.exists(path):
         print(f"The given path {path} does not exist.")
         return
@@ -46,9 +51,16 @@ def delete_dot_prefixed_folders(path):
         item_path = os.path.join(path, item)
         if os.path.isdir(item_path) and item.startswith('.'):
             shutil.rmtree(item_path)  # 删除以点开头的文件夹及其所有内容
-    print(f"Deleted folder: {path}\\.*")
+            logging.debug("Del: " + item_path)
+    logging.info(f"Deleted folder: {path}\\.*")
+
 
 qqnumber_folder_root = get_numeric_folders(root_folder)
 
 print(qqnumber_folder_root)
-# progress "nt_data" folder
+
+for i_qq_folder in qqnumber_folder_root:
+    i_qq_folder = i_qq_folder + "\\nt_qq"
+
+    del_path_all(i_qq_folder + "\\nt_data\\avatar")
+    del_path_all(i_qq_folder + "\\nt_data\\dataline")
